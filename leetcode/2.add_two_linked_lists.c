9a5b74b8da4ebdf3fd6a49979ee97e8bccd06f7d
iamrkg31 <rahul199431@gmail.com>
2018-10-09 12:43:43 +0530

    Updates

---
+++ leetcode/2.add_two_linked_lists.c
@@ -0,0 +1,93 @@
+// Example:
+
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+// Explanation: 342 + 465 = 807.
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    int carry = 0;
+    int sum = 0;
+    int flag = 0;
+    struct ListNode* res = l1;
+     
+    // when both of equal length
+    while(l1->next != NULL && l2->next != NULL){
+        sum = l1->val + l2->val + carry;
+        carry = sum/10;
+        if (sum >= 10) sum = sum%10; 
+        l1->val = sum;
+        l1 = l1->next;
+        l2 = l2->next;
+    }
+    
+    // when both of equal length
+    if (l1->next == NULL && l2->next == NULL){
+        sum = l1->val + l2->val + carry;
+        carry = sum/10;
+        if (sum >= 10) sum = sum%10; 
+        l1->val = sum;
+        if (carry > 0){
+            struct ListNode* carry_node  = (struct ListNode*)malloc(sizeof(struct ListNode));
+            carry_node->val = carry;
+            carry_node->next = NULL;
+            l1->next = carry_node;
+            l1 = l1->next;
+        }
+        flag = 1;
+    }
+    
+    // when l1 length is more than l2
+    if (l1->next != NULL && l2->next == NULL){
+        sum = l1->val + l2->val + carry;
+        carry = sum/10;
+        if (sum >= 10) sum = sum%10; 
+        l1->val = sum;
+        l1 = l1->next;
+    }
+    
+    // when l2 length is more than l1
+    if (l1->next == NULL && l2->next != NULL){
+        l1->next = l2->next;
+        l2->next = NULL;
+        sum = l1->val + l2->val + carry;
+        carry = sum/10;
+        if (sum >= 10) sum = sum%10; 
+        l1->val = sum;
+        l1 = l1->next;
+    }
+    
+    // when l1 and l2 length not equal
+    while(l1->next != NULL){
+        sum = l1->val + carry;
+        carry = sum/10;
+        if (sum >= 10) sum = sum%10; 
+        l1->val = sum;
+        l1 = l1->next;
+    }
+    
+    // when l1 and l2 length not equal, flag used for the same
+    if (l1->next == NULL && flag == 0){
+        sum = l1->val + carry;
+        carry = sum/10;
+        if (sum >= 10) sum = sum%10; 
+        l1->val = sum;
+        if (carry > 0){
+            struct ListNode* carry_node  = (struct ListNode*)malloc(sizeof(struct ListNode));
+            carry_node->val = carry;
+            carry_node->next = NULL;
+            l1->next = carry_node;
+            l1 = l1->next;
+        }
+    }
+
+return res;
+    
+}
