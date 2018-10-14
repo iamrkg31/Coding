af6a97785a0153552c5a8430f4980ebe78067e42
iamrkg31 <rahul199431@gmail.com>
2018-10-14 14:27:55 +0530

    Update

--- leetcode/4_median_two_sorted_arrays.c
+++ leetcode/4_median_two_sorted_arrays.c
@@ -15,57 +15,55 @@
 // The median is (2 + 3)/2 = 2.5
 
 
-double getMedian(int *nums, int numsSize, begin){
-    if (numsSize%2 != 0){
-        return (double)(nums[numsSize/2 + begin]);
+double getMedian(int *nums, int start, int end){
+    int length = end - start +1;
+    if (length%2 != 0){
+        return (double)(nums[start + length/2]);
     }else{
-        return (double)(nums[numsSize/2 - 1 + begin] + nums[numsSize/2 + begin])/2;
+        return (double)(nums[start + length/2 - 1] + nums[start + length/2])/2;
     }    
 }
 
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-    int res = 0;
-    int m1 = 0;
-    int m2 = 0;
-    
-    int flag = 0;
-    
     int i1 = 0;
     int i2 = nums1Size -1;
     int j1 = 0;
     int j2 = nums2Size - 1;
     
-    if (nums1Size > nums2Size){
-        flag = 1;
-    }else if(nums1Size < nums2Size){
-        flag = 2;
+    if(nums1Size == 0 && nums2Size == 0){ return 0.0;}
+    else if(nums1Size == 0){
+        return getMedian(nums2,j1,j2);
+    }else if (nums2Size == 0){
+        return getMedian(nums1,i1,i2);
     }
     
     while(1){
+        // printf("%d : %d\n",i1,i2);
+        // printf("%d : %d\n",j1,j2);
+        double m1 = getMedian(nums1, i1, i2);
+        double m2 = getMedian(nums2, j1, j2);
+        // printf("%f : %f\n",m1,m2);
         int size1 = i2-i1+1;
-        int size2 = j2-j1+1;
+        int size2 = j2-j1+1;        
         
-        if (size1 == 2 && size2 == 2){
-            if (nums1[])
-        }
-            
-            
-        m1 = getMedian(nums1, size1, i1);
-        m2 = getMedian(nums2, size2, j1);
+        if (m1 == m2) {
+            return m1;
+        } else if (size1 + size2 <= 4){   
+            // printf("%d\n",(nums1[i1]>nums2[j1]?nums1[i1]:nums2[j1]));
+            // printf("%d\n",(nums1[i2]<nums2[j2]?nums1[i2]:nums2[j2]));
+            return (double)((nums1[i1]>nums2[j1]?nums1[i1]:nums2[j1])+(nums1[i2]<nums2[j2]?nums1[i2]:nums2[j2]))/2;
+        }     
         
         if (m1 > m2){
-            i2 = size1/2;
-            j1 = size2/2;
+            i2 = i1 + size1/2;
+            j1 = j1 + size2/2;
         }else if(m1<m2){
-            i1 = size1/2;
-            j2 = size2/2;
-        }else{
-            res = m1;
-            break;
+            i1 = i1 + size1/2;
+            j2 = j1 + size2/2;
         }
         
     }
     
-    return res;
+    return ;
     
 }
